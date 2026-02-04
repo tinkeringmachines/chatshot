690b4df92455bcaa5c02520f80e429b72e5b2a84
Gobbo <gobbo[bot]@users.noreply.github.com>
2026-02-04 23:20:58 +0000

    Migrate from flake-utils to flake-parts
    flake-utils is deprecated, flake-parts is the modern modular approach.

--- flake.nix
+++ flake.nix
@@ -3,69 +3,69 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        # Node.js with dependencies
-        nodejs = pkgs.nodejs_22;
+      perSystem = { config, pkgs, system, ... }:
+        let
+          nodejs = pkgs.nodejs_22;
 
-        # Build the CLI package
-        chatshot = pkgs.buildNpmPackage {
-          pname = "chatshot";
-          version = "0.1.0";
-          src = ./.;
+          chatshot = pkgs.buildNpmPackage {
+            pname = "chatshot";
+            version = "0.1.0";
+            src = ./.;
 
-          npmDepsHash = "sha256-7E5lPB+XjrcVLusD67JsRQtX4Nqmi+zCMm3/qSgycrE=";
+            npmDepsHash = "sha256-7E5lPB+XjrcVLusD67JsRQtX4Nqmi+zCMm3/qSgycrE=";
 
-          nativeBuildInputs = [ pkgs.makeWrapper ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            buildInputs = [ pkgs.chromium ];
 
-          # Puppeteer needs chromium
-          buildInputs = [ pkgs.chromium ];
+            postInstall = ''
+              wrapProgram $out/bin/chatshot \
+                --set PUPPETEER_EXECUTABLE_PATH ${pkgs.chromium}/bin/chromium \
+                --set PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+            '';
 
-          postInstall = ''
-            wrapProgram $out/bin/chatshot \
-              --set PUPPETEER_EXECUTABLE_PATH ${pkgs.chromium}/bin/chromium \
-              --set PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
-          '';
+            meta = with pkgs.lib; {
+              description = "Generate realistic WhatsApp conversation screenshots";
+              license = licenses.mit;
+              mainProgram = "chatshot";
+            };
+          };
+        in
+        {
+          packages = {
+            default = chatshot;
+            chatshot = chatshot;
+          };
 
-          meta = with pkgs.lib; {
-            description = "Generate realistic WhatsApp conversation screenshots";
-            license = licenses.mit;
-            mainProgram = "chatshot";
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              nodejs
+              pkgs.chromium
+            ];
+
+            shellHook = ''
+              export PUPPETEER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
+              export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+              echo "ChatShot dev shell"
+              echo "Chromium: $PUPPETEER_EXECUTABLE_PATH"
+            '';
+          };
+
+          apps.default = {
+            type = "app";
+            program = "${chatshot}/bin/chatshot";
           };
         };
-      in
-      {
-        packages = {
-          default = chatshot;
-          chatshot = chatshot;
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            nodejs
-            pkgs.chromium
-          ];
-
-          shellHook = ''
-            export PUPPETEER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
-            export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-            echo "ChatShot dev shell"
-            echo "Chromium: $PUPPETEER_EXECUTABLE_PATH"
-          '';
-        };
-
-        apps.default = {
-          type = "app";
-          program = "${chatshot}/bin/chatshot";
-        };
-      }
-    );
+    };
 }
